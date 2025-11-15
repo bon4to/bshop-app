864ace725b18634cb33f8c09a1125d5541f8336f
Humberto Rodrigues <40726908+humbertowr@users.noreply.github.com>
2025-11-15 11:56:44 -0300

    Revert "refactor(frontend): reorganize project structure and remove nested fo…"

---
+++ frontend/bshop-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ButtonUsage from "./components/Button";
+
+function App() {
+  return (
+    <> 
+      <h1 className="text-xs">bshop-app</h1>
+      <ButtonUsage/>
+    </>
+  );
+}
+
+export default App;
